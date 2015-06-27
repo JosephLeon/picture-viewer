714f84c889aaf9decf7d8df40afdc23523d57f79
Joseph Leon <joseph.leon.9@gmail.com>
2015-06-27 10:52:06 -0400

    Add connection from image view object to the DetailViewController.

--- Project1/DetailViewController.swift
+++ Project1/DetailViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
 
+    @IBOutlet weak var detailImageView: UIImageView!
 
     var detailItem: AnyObject? {
         didSet {
